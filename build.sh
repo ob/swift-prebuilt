fc9ba54eede7329fb6b80eb5d62ab924bbe0971c
Oscar Bonilla <6f6231@gmail.com>
2019-08-21 13:57:44 -0700

    Update docs

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ set -euo pipefail
 SWIFT_SOURCE=swift-source
 
 cd $SWIFT_SOURCE
+# If you change these flags, you will need to change the BUILD
+# directory in the packaging script.
 ./swift/utils/build-script --release-debuginfo
